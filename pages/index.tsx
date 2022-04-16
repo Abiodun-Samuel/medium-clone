b071f2af695fa15612b03fb99ac57ec69ca814ae
Abiodun Samuel <abiodunsamyemi@gmail.com>
2022-04-16 01:21:25 +0100

    building post section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,16 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
+import Link from 'next/link'
 import Header from '../components/Header'
-import { sanityClient } from '../sanity'
+import { sanityClient, urlFor } from '../sanity'
+import {Post} from '../typings'
 interface Props {
-  posts: 
+  posts: [Post]
 }
 
-const Home: NextPage = ({posts}) => {
+export default function Home({ posts }: Props) {
+  console.log(posts);
+
   return (
     <div className="max-w-7xl mx-auto">
       <Head>
@@ -25,6 +29,22 @@ const Home: NextPage = ({posts}) => {
       </div>
 
       {/* Posts  */}
+      <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6'>
+        {posts.map((post) => (
+          <Link key={post._id} href={`/post/${post.slug.current}`}>
+            <div className=''>
+              <img src={urlFor(post.mainImage).url()!} alt={post.title} />
+              <div className='flex justify-between p-5 bg-white'>
+                <div>
+                  <p>{ post.title}</p>
+                  <p>{post.description} by {post.author.name}</p>
+                </div>
+                  <img className='h-12 w-12 rounded-full' src={urlFor(post.author.image).url()!} alt={post.title} />
+              </div>
+            </div>
+          </Link>
+        ))}
+      </div>
     </div>
   )
 }
@@ -52,4 +72,3 @@ export const getServerSideProps = async () => {
 
 }
 
-export default Home
